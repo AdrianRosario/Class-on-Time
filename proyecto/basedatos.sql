71ce0a59ef11d9512875ba91ddf13f52ea60f4a4
Adrian <adrianrosario660@gmail.com>
2023-05-05 21:29:11 -0400

    actulizando basedatos en el proyecto

--- proyecto/basedatos.sql
+++ proyecto/basedatos.sql
@@ -1,6 +1,21 @@
+
+tabla del backend
 create table tareas(
-idcreador integer,
-nombe_tareas varchar(200),
-	descripcion varchar(300)
+ idcreador integer,
+ nombe_tareas varchar(200),
+ descripcion varchar(300)
+ idtares integer,
+ tipos varchar(200),
+ PRIMARY KEY (idtares)
 )
 
+--  tabla de login
+create table prueba(
+	id integer,
+	nombre varchar(200),
+	email varchar(200),
+	password varchar(200),
+	token varchar(200),
+	PRIMARY KEY (id)
+
+)
